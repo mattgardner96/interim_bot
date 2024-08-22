2830972a6fe83bb34538a039d699ba9d0a4e4efb
Matt G <mattgardner96@gmail.com>
2024-08-22 13:01:32 -0400

    initial motor class implementation

--- ros2_ws/src/BotServer/BotServer/motor.py
+++ ros2_ws/src/BotServer/BotServer/motor.py
@@ -9,6 +9,7 @@ import time
 ENCODER_PPR=100 # 100 pulses per revolution
 PWM_FREQ=25000 # 25kHz PWM frequency
 PWM_FULL_SCALE = 255 # 8-bit PWM
+WHEEL_DIAMETER = 0.064 # wheel diameter in meters
 
 class Motor_24H():
     
@@ -21,7 +22,7 @@ class Motor_24H():
         dir_pin: BCM pin number for direction signal
         enc_a_pin: BCM pin number for encoder channel A
         enc_b_pin: BCM pin number for encoder channel B
-        forward_dir: 1 for CW, -1 for CCW
+        forward_dir: 1 for CW, 0 for CCW
         '''
         
         init_done = False
@@ -35,37 +36,63 @@ class Motor_24H():
         self.forward_dir = forward_dir
         
         # init the pins and their init states
-        self.pi.set_mode(self.pwm_pin, pigpio.OUTPUT)
+        self.pi.set_mode(self.pwm_pin, pigpio.ALT0)
         self.pi.set_mode(self.dir_pin, pigpio.OUTPUT)
         self.pi.set_mode(self.enc_a_pin, pigpio.INPUT)
         self.pi.set_mode(self.enc_b_pin, pigpio.INPUT)
         
         # init PWM to off
         self.pi.set_PWM_range(self.pwm_pin,PWM_FULL_SCALE)
+        self.pi.set_PWM_frequency(self.pwm_pin, PWM_FREQ)
         self.pi.write(self.pwm_pin, PWM_FULL_SCALE) # inverted; this is actually off
         
-        self.pi.write(self.dir_pin, self.forward_dir >)
+        # init direction to forward
+        self.pi.write(self.dir_pin, self.forward_dir)
         
-    def set_duty_inv(duty:float)->bool:
+    def set_duty_inv(self,duty:float)->bool:
             '''
-            duty: 0-100, duty cycle percentage
-            translates to inverted duty cycle autom
+            duty percent, not inverted
             '''
             duty = 255 * (100 - duty) / 100
             
-            return self.pi.set_PWM_dutycycle(self.pwm_pin, duty)
+            return self.set_PWM_dutycycle(self.pwm_pin, duty)
         
-        # init PWM
-        self.pi.set_PWM_frequency(self.pwm_pin, PWM_FREQ)
-        self.pi.set_duty(self.pwm_pin, 0) # this is stopped for my motors
+    def get_duty_inv(self)->float:
+        '''
+        returns the inverted duty cycle as a percentage
+        '''
+        duty = self.pi.get_PWM_dutycycle(self.pwm_pin)
+        return 100 - (duty / 255 * 100)
         
     
     def set_velocity(self, velocity:float):
         '''
         velocity: velocity in m/s
         '''
+        pass
         
     def __del__(self):
+        self.pi.set_PWM_dutycycle(self.pwm_pin, 255) # inverted; this is actually off
+
+def main():
+    pi = pigpio.pi()
+    left_motor = Motor_24H(pi,13,26,17,27,0)
+    right_motor = Motor_24H(pi,12,16,23,24,1)
+    
+    left_motor.set_duty_inv(5)
+    right_motor.set_duty_inv(5)
+    
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        left_motor.set_duty_inv(0)
+        right_motor.set_duty_inv(0)
+    
+
+if __name__ == '__main__':
+    main()
+        
         
         
         
--- ros2_ws/src/BotServer/BotServer/mover.py
+++ ros2_ws/src/BotServer/BotServer/mover.py
@@ -1,3 +1,24 @@
+import rclpy
+from rclpy.node import Node
+import pigpio
+
+from motor import Motor_24H
+
+
+class Mover(Node):
+    def __init__(self):
+        super().__init__('mover')
+        pi = pigpio.pi()
+        left_motor = Motor_24H(pi,13,26,17,27,-1)
+        right_motor = Motor_24H(pi,12,16,23,24,1)
+        vel_cmd_srv = self.create_service(Velocity, 'velocity_cmd', self.velocity_cmd_callback)
+    
+    def velocity_cmd_callback(self, request, response):
+        left_motor.set_velocity(request.left_velocity)
+        right_motor.set_velocity(request.right_velocity)
+        response.success = True
+        return response
+
 def main():
     print('Hi from BotServer.')
 
